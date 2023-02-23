0459ef2bd69542355946e378adb41914c56c770e
Evgenii Sendziuk <evgeniisendziuk@taxdome.com>
2023-02-23 17:38:45 +0300

    Initial commit

---
+++ spec/mvc_one_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MvcOne do
+  it "has a version number" do
+    expect(MvcOne::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
